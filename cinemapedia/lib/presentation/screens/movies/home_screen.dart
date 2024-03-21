5e3a1f8b6858ceb1722305902f064c2e086332a6
David Ramos <davidrtornay@hotmail.com>
2024-03-21 10:06:05 +0100

    Creación de environment

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,13 +1,17 @@
+import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
-  static const name="home-screen";
-  const HomeScreen({super.key});  
+  static const name = "home-screen";
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Placeholder(),
+      body: Center(
+        child: Text(Environment.theMovieDbKey),
+      ),
     );
   }
 }
