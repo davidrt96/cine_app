378aa481a19ce9f5103c5e471b3b5a2290615549
David Ramos <davidrtornay@hotmail.com>
2024-03-21 12:50:57 +0100

    Probider

---
+++ cinemapedia/lib/presentation/providers/movies/movies_respository_provider.dart
@@ -0,0 +1,9 @@
+import 'package:cinemapedia/infraestructure/datasources/moviedb_datasource.dart';
+import 'package:cinemapedia/infraestructure/respositories/movie_respository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+//Este repositorio es inmutable, solo lectura, proporcionar información
+final movieRepositoryProvider = Provider((ref) {
+  return MovieRepositoryImpl(MoviedbDatasource());
+});
